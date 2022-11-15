b0ddc70f5af9a68854278ca474deb68334b8f488
Isayama_Kagura <isayama_kagura@qq.com>
2022-11-15 11:37:21 +0800

    优化主窗口拖拽

--- TsubakiTranslator/MainWindow.xaml.cs
+++ TsubakiTranslator/MainWindow.xaml.cs
@@ -19,7 +19,36 @@ namespace TsubakiTranslator
 
         private void ColorZone_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            base.DragMove();//实现整个窗口的拖动
+            /* 如何在Window.ResizeMode属性为CanResize的时候，阻止窗口拖动到屏幕边缘自动最大化。
+              (When the Window.ResizeMode property is CanResize, 
+              when the window is dragged to the edge of the screen, 
+              it prevents the window from automatically maximizing.)*/
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (Mouse.LeftButton == MouseButtonState.Pressed)
+                {
+                    var windowMode = this.ResizeMode;
+                    if (this.ResizeMode != ResizeMode.NoResize)
+                    {
+                        this.ResizeMode = ResizeMode.NoResize;
+                    }
+
+                    this.UpdateLayout();
+
+
+                    /* 当点击拖拽区域的时候，让窗口跟着移动
+                    (When clicking the drag area, make the window follow) */
+                    DragMove();
+
+
+                    if (this.ResizeMode != windowMode)
+                    {
+                        this.ResizeMode = windowMode;
+                    }
+
+                    this.UpdateLayout();
+                }
+            }
         }
 
 
